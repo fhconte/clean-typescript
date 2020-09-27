5fe01e7eb934e08e0c154fd15b96d8c2403e9d92
Flavio Conte <flaviohconte@gmail.com>
2020-09-27 18:05:24 -0300

    refactor: move axios mock to a separate file

---
+++ src/data/test/mock-http-post.ts
@@ -0,0 +1,8 @@
+import faker from 'faker'
+
+import { HttpPostParams } from '../protocols/http/http-post-client'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
